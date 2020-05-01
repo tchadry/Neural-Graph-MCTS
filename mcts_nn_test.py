557b44bce0b0db30d02bae89be255fc7b389c77f
Nicolae Lari <nl2605@columbia.edu>
2020-05-01 18:07:37 -0400

    Fixed mcts_nn

--- mcts_nn_test.py
+++ mcts_nn_test.py
@@ -24,7 +24,7 @@ args = dotdict({
     'numItersForTrainExamplesHistory': 20,
     # 'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
     'device': 'cpu',
-    'n_nodes': 8,
+    'n_nodes': 25,
     'n_node_features': 5,
     'n_executions': 100,
 
@@ -81,6 +81,7 @@ def predict_path(game, simulations, nnet=None):
 
 
 n_games = 100
+error = 1.01
 n_nodes = args.n_nodes
 
 games, optimal = create_mcts_games(n_games, n_nodes)
@@ -88,11 +89,15 @@ games, optimal = create_mcts_games(n_games, n_nodes)
 # temp4 chkpnt1, 8 nodes
 # [0.01, 0.01, 0.03, 0.05, 0.07, 0.13, 0.14, 0.11, 0.16, 0.21, 0.32, 0.29, 0.41, 0.46, 0.5, 0.43, 0.53, 0.71, 0.69, 0.78, 0.69, 0.74, 0.85, 0.77]
 
-num_simulations = [5, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000]#, 1500, 2000, 2500]
+num_simulations = [5, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500]
 
 #files = ['best.pth.tar', 'checkpoint_1.pth.tar', 'checkpoint_2.pth.tar', 'checkpoint_4.pth.tar', 'checkpoint_8.pth.tar', 'none']
-files = ['checkpoint_1.pth.tar', 'none']
+#files = ['checkpoint_1.pth.tar', 'none', 'checkpoint_50sims_1.pth.tar']
+files = ['50sims_25nodes.pth.rar', 'none']
 
+print(f'games: {n_games}\n'
+      f'error: {error}\n'
+      f'nodes: {args.n_nodes}')
 
 for file in files:
     print(f'File: {file}')
@@ -116,7 +121,7 @@ for file in files:
         for i in range(n_games):
             predicted_cost = predict_path(games[i],  sim, net)
 
-            if optimal[i]*1.1 >= predicted_cost:
+            if optimal[i]*error >= predicted_cost:
                 wins += 1
 
         result.append(wins/n_games)
