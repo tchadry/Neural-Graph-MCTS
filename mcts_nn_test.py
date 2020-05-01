a3b107bbf591005a9311593b991f2e1f64e2f887
Nicolae Lari <nl2605@columbia.edu>
2020-05-01 19:06:15 -0400

    Small changes

--- mcts_nn_test.py
+++ mcts_nn_test.py
@@ -24,7 +24,7 @@ args = dotdict({
     'numItersForTrainExamplesHistory': 20,
     # 'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
     'device': 'cpu',
-    'n_nodes': 25,
+    'n_nodes': 8,
     'n_node_features': 5,
     'n_executions': 100,
 
@@ -89,11 +89,11 @@ games, optimal = create_mcts_games(n_games, n_nodes)
 # temp4 chkpnt1, 8 nodes
 # [0.01, 0.01, 0.03, 0.05, 0.07, 0.13, 0.14, 0.11, 0.16, 0.21, 0.32, 0.29, 0.41, 0.46, 0.5, 0.43, 0.53, 0.71, 0.69, 0.78, 0.69, 0.74, 0.85, 0.77]
 
-num_simulations = [5, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500]
+num_simulations = [5, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900]#, 1000, 1500, 2000, 2500]
 
 #files = ['best.pth.tar', 'checkpoint_1.pth.tar', 'checkpoint_2.pth.tar', 'checkpoint_4.pth.tar', 'checkpoint_8.pth.tar', 'none']
 #files = ['checkpoint_1.pth.tar', 'none', 'checkpoint_50sims_1.pth.tar']
-files = ['50sims_25nodes.pth.rar', 'none']
+files = ['checkpoint_1.pth.tar', 'none']
 
 print(f'games: {n_games}\n'
       f'error: {error}\n'
@@ -105,7 +105,7 @@ for file in files:
 
     result = []
 
-    args.checkpoint = './temp4/'
+    args.checkpoint = './temp_models/'
     net = nn(args)
 
     if file != 'none':
