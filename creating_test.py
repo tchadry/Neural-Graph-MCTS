04eba569e81be5b37541151c15c106d844756d5d
felipegrabowsky <fm2579@columbia.edu>
2020-05-02 13:49:56 -0300

    uploading test file

--- creating_test.py
+++ creating_test.py
@@ -22,10 +22,10 @@ def create_mcts_games(num_games, nodes):
     return games_10_nodes, optimal_results
 
 
-games, optimal= create_mcts_games(num_games=1, nodes=10)
+games, optimal= create_mcts_games(num_games=20, nodes=12)
 
 
 
 
-with open('10nodestestfile', 'wb') as file:
+with open('12nodestestfile2', 'wb') as file:
      pickle.dump(games,file)
