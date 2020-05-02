17289db09478f51855184b5f3b2607a947a35578
felipegrabowsky <fm2579@columbia.edu>
2020-05-02 13:15:49 -0300

    updated creating test

--- creating_test.py
+++ creating_test.py
@@ -8,27 +8,24 @@ def create_mcts_games(num_games, nodes):
     returns list of games and list of optimal game values
     """
 
-    games = []
-    optimal_results = []
+    games_10_nodes={}
+    optimal_results=[]
 
     for i in range(num_games):
         print(i)
         print(i)
         current = TspGame(nodes)
         optimal_res, _ = current.optimal_solution()
-        games.append(current)
+        games_10_nodes[current]=optimal_res
         optimal_results.append(optimal_res)
         
-    return games, optimal_results
+    return games_10_nodes, optimal_results
 
 
-games, optimal= create_mcts_games(num_games=20, nodes=10)
-
-games_10_nodes={}
-
-for i,j in games,optimal:
-    games_10_nodes[i]=j
+games, optimal= create_mcts_games(num_games=1, nodes=10)
 
 
-with open('10nodestest.txt', 'w') as file:
-     file.write(pickle.dumps(games_10_nodes))
+
+
+with open('10nodestestfile', 'wb') as file:
+     pickle.dump(games,file)
