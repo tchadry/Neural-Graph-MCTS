d4ae8672b4efbacf02d27a38b03e0f1c0f39fe70
Nicolae Lari <nl2605@columbia.edu>
2020-05-03 19:00:00 -0400

    Improved multiprocessing

--- multi_mcts_nn_test.py
+++ multi_mcts_nn_test.py
@@ -6,6 +6,22 @@ from util import dotdict
 from TspNN import NNetWrapper as nn
 from util import dotdict
 from multiprocessing import Pool
+import multiprocessing
+import multiprocessing.pool
+
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+    def _set_daemon(self, value):
+        pass
+    daemon = property(_get_daemon, _set_daemon)
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
 
 
 initial_args = dotdict({
@@ -81,28 +97,63 @@ def predict_path(game, simulations, args, nnet=None):
     return  game.path_pay(tuple(path[1:]))
 
 
-
-n_games = 75
-error = 1.01
 n_nodes = initial_args.n_nodes
 
-games, optimal = create_mcts_games(n_games, n_nodes)
-
-# temp4 chkpnt1, 8 nodes
-# [0.01, 0.01, 0.03, 0.05, 0.07, 0.13, 0.14, 0.11, 0.16, 0.21, 0.32, 0.29, 0.41, 0.46, 0.5, 0.43, 0.53, 0.71, 0.69, 0.78, 0.69, 0.74, 0.85, 0.77]
-
-num_simulations = [5, 25, 50]#, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000]#, 2500]
-
-#files = ['best.pth.tar', 'checkpoint_1.pth.tar', 'checkpoint_2.pth.tar', 'checkpoint_4.pth.tar', 'checkpoint_8.pth.tar', 'none']
-#files = ['checkpoint_1.pth.tar', 'none']
-
-files = ['inv0_1.tar', 'inv0_2.tar', 'inv0_3.tar', 'inv1_1.tar', 'inv1_2.tar', 'inv1_3.tar']
-
-print(f'games: {n_games}\n'
-      f'error: {error}\n'
-      f'nodes: {initial_args.n_nodes}')
-
 results = {}
+
+##########################################################################################################
+# Last level parallelization
+
+global_net = None
+global_args = None
+global_sim = None
+
+def run_game(i):
+
+    predicted_cost = predict_path(games[i], global_sim, global_args, global_net)
+
+    return optimal[i] * error >= predicted_cost
+
+
+
+def last_test_model(file):
+    print(f'File: {file}')
+
+    global global_args
+    global global_net
+    global global_sim
+
+    global_args = initial_args
+    global_args.checkpoint = './temp_models/'
+    global_net = nn(global_args)
+
+    result = []
+
+    if file != 'none':
+        global_net.load_checkpoint(global_args.checkpoint, filename=file)
+    else:
+        global_net = None
+
+    for sim in num_simulations:
+
+        #print("testing simulations", sim)
+
+        global_sim = sim
+
+        pool = Pool()
+        game_results = pool.map(run_game, list(range(n_games)))
+        pool.close()
+        pool.join()
+
+
+
+        result.append(sum(game_results) / n_games)
+
+    #print(result)
+
+    return result
+
+############################################################################################################
 
 def test_model(file):
     args = initial_args
@@ -137,26 +188,106 @@ def test_model(file):
 
     return (file, result)
 
-    #results[file] = result
+#######################################################################################################################
+# Multiprocessing for num_simulations
 
-# for file in files:
-#     test_model(file)
-#     #print("Simulations done")
-#     #print("Hello")
-#     #plt.title("Plotting MCTS percentage within 1.1 of optimal with change in iterations")
-#     #plt.xlabel('Number ofsimulations')
-#     #plt.ylabel('MCTS Results')
-#     #plt.plot(num_simulations, result)
-#     #plt.show()
+def run_simulation(arguments):
+
+    sim, args, file = arguments
+
+    args.numMCTSSims = sim
+
+    wins = 0
+
+    # Load net
+    net = nn(args)
+    if file != 'none':
+        net.load_checkpoint(args.checkpoint, filename=file)
+    else:
+        net = None
+
+    # Simulate n games
+    for i in range(n_games):
+        predicted_cost = predict_path(games[i], sim, args, net)
+
+        if optimal[i] * error >= predicted_cost:
+            wins += 1
+
+    return (sim, wins / n_games)
 
 
-parallel = False
+#global_list = []
 
-if parallel:
-    pool = Pool(6)
-    tuple_results = pool.map(test_model, files)
-    for t in tuple_results:
-        results[t[0]] = t[1]
+def multi_test_model(file):
+    #print(file, sim_pools)
+
+
+    args = initial_args
+
+    args.checkpoint = './temp_models/'
+
+
+    new_pool = Pool(sim_pools)
+
+    tuple_results = new_pool.map(run_simulation, list(zip(num_simulations, [args]*len(num_simulations), [file]*len(num_simulations))))
+
+    #tuple_results = new_pool.map(run_simulation, global_list)
+
+
+    # Sort by number of simulations performed
+    tuple_results = sorted(tuple_results, key=lambda x: x[0])
+    result = [x[1] for x in tuple_results]
+
+    return (file, result)
+#######################################################################################################################
+n_games = 100
+error = 1.01
+
+games, optimal = create_mcts_games(n_games, n_nodes)
+
+
+num_simulations = [5, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500]#, 600, 700, 800]#, 900, 1000, 1500, 2000]#, 2500]
+
+# NB: include 'none' as a file for mcts only evaluaton
+files = ['inv0_1.tar', 'inv0_2.tar', 'none']
+
+print(f'games: {n_games}\n'
+      f'error: {error}\n'
+      f'nodes: {initial_args.n_nodes}')
+
+
+
+
+parallel_models = True
+parallel_simulations = True
+last_level = True
+cpu_count = multiprocessing.cpu_count()
+pools = len(files)
+sim_pools = (cpu_count - pools) // pools
+
+
+if last_level:
+    for file in files:
+        result = last_test_model(file)
+        results[file] = result
+elif parallel_models:
+    # Only assign each cpu to a pool
+    if cpu_count < 3*len(files) or not parallel_simulations:
+        pool = Pool()
+        tuple_results = pool.map(test_model, files)
+        for t in tuple_results:
+            results[t[0]] = t[1]
+
+        pool.close()
+        pool.join()
+
+    else:
+        pool = MyPool(pools)
+        tuple_results = pool.map(multi_test_model, files)
+        for t in tuple_results:
+            results[t[0]] = t[1]
+        pool.close()
+        pool.join()
 else:
     for file in files:
         result = test_model(file)
