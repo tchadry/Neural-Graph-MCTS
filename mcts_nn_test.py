7bf1b23eb3d63f1fdfbfe684c50df4ed83518ac7
tchadry <tchadrycki@gmail.com>
2020-05-03 15:29:02 -0400

    import args from main training file

--- mcts_nn_test.py
+++ mcts_nn_test.py
@@ -7,40 +7,6 @@ from TspNN import NNetWrapper as nn
 from util import dotdict
 from main import args
 
-<<<<<<< HEAD
-args = dotdict({
-    'numIters': 10,
-    'numEps': 50,  # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 15,  #
-    'updateThreshold': 0.55,
-# During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'maxlenOfQueue': 200000,  # Number of game examples to train the neural networks.
-    'numMCTSSims': 50,  # Number of games moves for MCTS to simulate.
-    'arenaCompare': 40,  # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1,
-    'cuda': False,
-
-    'checkpoint': './10nodesFINAL/',
-    'load_model': False,
-    'load_folder_file': ('./10nodesFINAL/', 'best.pth.tar'),
-    'numItersForTrainExamplesHistory': 20,
-    # 'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-    'device': 'cpu',
-    'n_nodes': 10,
-    'n_node_features': 5,
-    'n_executions': 100,
-
-    'lr': 0.001,
-    'dropout': 0.3,
-    'epochs': 10,
-    'batch_size': 64,
-    'use_gdc': True,
-    'invert_probs': False
-
-
-})
-=======
->>>>>>> 24da54930c60f6ce0929cdf8f4ef7d61a183d316
 
 def create_mcts_games(num_games, nodes=8):
     """
@@ -82,20 +48,15 @@ def predict_path(game, simulations, nnet=None):
     return  game.path_pay(tuple(path))
 
 
-
 n_games = 10
 n_nodes = args.n_nodes
 
 games, optimal = create_mcts_games(n_games, n_nodes)
 
-<<<<<<< HEAD
 # temp4 chkpnt1, 8 nodes
 # [0.01, 0.01, 0.03, 0.05, 0.07, 0.13, 0.14, 0.11, 0.16, 0.21, 0.32, 0.29, 0.41, 0.46, 0.5, 0.43, 0.53, 0.71, 0.69, 0.78, 0.69, 0.74, 0.85, 0.77]
 
 num_simulations = [500,1000,1500,2000]# 1500, 2000, 2500]
-=======
-num_simulations = np.arange(100, 3000, 200)
->>>>>>> 24da54930c60f6ce0929cdf8f4ef7d61a183d316
 
 files = ['best.pth.tar', 'none']
 
@@ -105,11 +66,8 @@ for file in files:
     print('################################################################################')
 
     result = []
-<<<<<<< HEAD
 
     args.checkpoint = './10nodesFINAL/'
-=======
->>>>>>> 24da54930c60f6ce0929cdf8f4ef7d61a183d316
     net = nn(args)
 
     if file != 'none':
